4c396fd2321e643ab701a92625b49256393c9e94
Ilse Tastenhoye <ilse.tastenhoye@gmail.com>
2024-08-16 00:06:16 +0200

    script.js receiving data - first test

--- src/main/resources/static/javascript/script.js
+++ src/main/resources/static/javascript/script.js
@@ -1,1 +1,11 @@
 console.log("Hello there!")
+
+document.addEventListener("DOMContentLoaded",function(){
+    fetch('http://localhost:8080/betalingen/voedsel')
+        .then(resp => resp.json())
+        .then(data => {console.log("Data ontvangen: ", data);
+        })
+        .catch(error => {console.error("Fout bij ophalen: ", error);
+        });
+});
+
